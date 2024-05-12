ef43c8e0c7d2fc673c1caa67ba9ddec60042af44
LUCAS BEZERRA DA CRUZ <lucasbezerracrz@gmail.com>
2024-05-12 12:07:59 -0300

    Add files via upload

--- dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private long id;
@@ -19,7 +20,13 @@ public class GameMinDTO {
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
-
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
 	public long getId() {
 		return id;
 	}
